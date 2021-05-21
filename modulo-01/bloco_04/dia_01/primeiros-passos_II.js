12a1bfd37d8034acdaada822cbc7dec8e7fe1e1a
Cassyel Oliveira <cassyel1967@gmail.com>
2021-05-21 19:10:25 -0300

    Realiza exercicios do conteudo do dia 1, bloco 4

---
+++ modulo-01/bloco_04/dia_01/primeiros-passos_II.js
@@ -0,0 +1,27 @@
+const name = "Cassyel";
+const birthCity = 'Porto Alegre';
+let birthYear = 2002;
+
+console.log(name)
+console.log(birthCity)
+console.log(birthYear)
+
+birthYear = 2030;
+
+console.log(birthYear)
+
+birthCity = 'Viamão';
+
+console.log(birthCity)
+
+// Da  erro porque 'birthCity' é uma constante já declarada, então não podemos mudar seu valor
+
+
+/* 
+1. Crie uma constante chamada name e atribua a ela o seu nome (Exemplo: Carolina);
+2. Crie uma constante chamada birthCity e atribua a ela a sua cidade natal;
+3. Crie uma variável chamada birthYear e atribua a ela o ano em que você nasceu;
+4. Utilize o console.log() para imprimir as constantes e variáveis que você criou;
+5. Altere o valor atribuído à variável birthYear para 2030. Faça um console.log(birthYear) novamente para ver o que acontece!
+6. Altere o valor atribuído à constante birthCity . Faça um console.log(birthCity) novamente! Você saberia explicar por que recebemos uma mensagem de erro? 🤔
+ */
