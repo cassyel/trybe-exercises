b0f7efbcd99b59f059c7cc7ca118aa5040d5899d
Cassyel <cassyel1967@gmail.com>
2021-06-18 15:11:54 -0300

    Realiza exercicios dia 1, bloco 7

---
+++ modulo-01/bloco_07/dia_01/Prática/script-II.js
@@ -0,0 +1,36 @@
+// Exercicio 1
+
+const factorial = number => {
+  let result = 1
+
+  for (let index = 2; index <= number; index += 1) {
+      result *= index
+  }
+
+  return result
+}
+
+console.log(factorial(5))
+
+// Recursiva
+const factorial = number => number > 1 ? number * factorial(number - 1) : 1
+console.log(factorial(5))
+
+// Exercicio 2
+
+const longestWord = text => {
+  let wordArray = text.split(' ')
+  let maxLength = 0
+  let result = ''
+
+  for (const word of wordArray) {
+      if (word.length > maxLength) {
+          maxLength = word.length
+          result = word
+      }
+  }
+
+  return result
+}
+
+console.log(longestWord("Antonio foi no banheiro e não sabemos o que aconteceu"))
