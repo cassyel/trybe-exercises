327e3316d573a2f4cb5fab6c1e86e1ddb337d555
Cassyel <cassyel1967@gmail.com>
2021-07-14 13:29:58 -0300

    Exercicios dia 1, bloco 10

---
+++ modulo-01/bloco_10/dia_01/sum.js
@@ -0,0 +1,10 @@
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
+
+  return a + b;
+}
+
+const sum = module.exports
+// implemente seus testes aqui
