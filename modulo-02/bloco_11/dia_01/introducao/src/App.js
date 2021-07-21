7b2620f40a57178b294224537051566d9787b39c
Cassyel <cassyel1967@gmail.com>
2021-07-21 18:36:44 -0300

    Realiza exercicios dia 1, bloco 11

---
+++ modulo-02/bloco_11/dia_01/introducao/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Firstcomponent from "./Componets";
+
+class App extends React.Component{
+  render() {
+    return (
+      <Firstcomponent />
+    );
+  }
+}
+
+export default App;
