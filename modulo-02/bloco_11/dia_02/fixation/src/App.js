2c2be9651f30a47c4002d7211a0f4367330d4152
Cassyel <cassyel1967@gmail.com>
2021-07-23 10:28:59 -0300

    Realiza exercicios dia 2, bloco 11

---
+++ modulo-02/bloco_11/dia_02/fixation/src/App.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+import Greeting from "./Greeting";
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Greeting name="Cassyel" lastName="Oliveira" />
+      </div>
+    )
+  }
+}
+
+
+
+export default App;
